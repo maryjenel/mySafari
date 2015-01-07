485b2336ef6946e1500eacf460a41a979a440c15
Mary Jenel <mmyers7@hawaii.edu>
2015-01-07 12:30:59 -0800

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Mary Jenel Myers on 1/7/15.
+//  Copyright (c) 2015 Mary Jenel Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Mary Jenel Myers on 1/7/15.
+//  Copyright (c) 2015 Mary Jenel Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
